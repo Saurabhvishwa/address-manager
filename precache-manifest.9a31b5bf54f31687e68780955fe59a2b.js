632fe31cfa95aabc4570f37d4bae7c4a283c3502
Saurabh <saurabhvkrma@gmail.com>
2020-06-13 13:49:56 +0530

    Updates

---
+++ precache-manifest.9a31b5bf54f31687e68780955fe59a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce94ec8af297f141d096fb51525a62d6",
+    "url": "/address-manager/index.html"
+  },
+  {
+    "revision": "8a249bb7ad94f04e7019",
+    "url": "/address-manager/static/css/main.603bb7ea.chunk.css"
+  },
+  {
+    "revision": "82e0e52aa11cd51931f7",
+    "url": "/address-manager/static/js/2.1390d6fe.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/address-manager/static/js/2.1390d6fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a249bb7ad94f04e7019",
+    "url": "/address-manager/static/js/main.f064280a.chunk.js"
+  },
+  {
+    "revision": "7cab3496a069979abca3",
+    "url": "/address-manager/static/js/runtime-main.708a091c.js"
+  }
+]);
